2b97e92ec1278bd6640da78b7d6b37422de70880
Bryn <brynphillip1@gmail.com>
2018-11-24 17:52:09 -0800

    A couple fixes.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
-#include <iostream>
 
-int main() {
+#include <iostream>
 #include "node.h"
 #include "freeList.h"
 #include "listInterface.h"
 #include "linkedList.h"
 #include "utilities.h"
 //#include <cassert>
+
     int main(int argc, char *argv[]) {
+
         LList<int> test(0);
+        //return a.exec();
         for (int i = 0; i < 10; i++)
             test.insert(i); // adding 10 values. After this operation the curr (current pointer will be ath the tail)
         for (int i = 0; i < 10; i++)
-            cout << "Removed " << 9 - i << " th value " << test.remove() << endl; // removing 10 values
+            std::cout << "Removed " << 9 - i << " th value " << test.remove() << endl; // removing 10 values
     }
-
-}
